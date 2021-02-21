35c370ba67e3f890bb133d8199f2cf8e2d0bfea6
DevinAlexander <devinmalexander@gmail.com>
2021-02-21 13:17:12 -0600

    [CHORE] Create User model (with properties)

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string phoneNumber { get; set; }
+        public string adminStatus { get; set; }
+    }
+}
